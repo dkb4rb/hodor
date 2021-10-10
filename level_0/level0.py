f66ad7b231a235360932da166ef76021ad5d1a62
Juan Duque <84897255+dkb4rb@users.noreply.github.com>
2021-10-10 03:28:59 -0500

    Update level0.py

--- level_0/level0.py
+++ level_0/level0.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# levelo.py.py
+# level0.py
 # Juan Duque <3428@holbertonschool.com>
 """ Import libraries necesary for execute program
 """
-import requests
+import requests, os
 url = "http://158.69.76.135/level0.php"
 data_vote = { 'id':'26', 'holdthedoor':'Submit'}
 
@@ -11,3 +11,4 @@ if __name__ == "__main__":
     for i in range(0, 1024):
         requests.post(url, data=data_vote)
         print("[*] Se a enviado Peticion {}".format(i))
+        os.system("clear")
